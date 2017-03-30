2765d20c3ce2b06e6ba296648988e9fb9ff46f7e
ReubenDomacasse <0918901@hr.nl>
2017-03-30 19:01:45 +0200

    Working Code Serial monitor to Java

---
+++ src/Automaat.java
@@ -0,0 +1,95 @@
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent;
+import gnu.io.SerialPortEventListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Enumeration;
+
+
+public class Automaat implements SerialPortEventListener{
+
+    SerialPort serialPort;
+    Automaat automaat=null;
+    /** The port we're normally going to use. */
+    private static final String PORT_NAMES[] = {"COM1","COM2","COM3","COM4","COM5"};
+    /**
+     * A BufferedReader which will be fed by a InputStreamReader
+     * converting the bytes into characters
+     * making the displayed results codepage independent
+     */
+    private BufferedReader input;
+    /** The output stream to the port */
+    private OutputStream output;
+    /** Milliseconds to block while waiting for port open */
+    private static final int TIME_OUT = 3000;
+    /** Default bits per second for COM port. */
+    private static final int DATA_RATE = 9600;
+
+    public void initialize() {
+        CommPortIdentifier portId = null;
+        Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
+        //First, Find an instance of serial port as set in PORT_NAMES.
+        while (portEnum.hasMoreElements()) {
+            CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
+            for (String portName : PORT_NAMES) {
+                if (currPortId.getName().equals(portName)) {
+                    portId = currPortId;
+                    break;
+                }
+            }
+        }
+        if (portId == null) {
+            System.out.println("Could not find COM port.");
+            return;
+        }
+        try {
+            // open serial port, and use class name for the appName.
+            serialPort = (SerialPort) portId.open(this.getClass().getName(),
+                    TIME_OUT);
+            // set port parameters
+            serialPort.setSerialPortParams(DATA_RATE,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+            // open the streams
+            input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
+            output = serialPort.getOutputStream();
+            // add event listeners
+            serialPort.addEventListener(this);
+            serialPort.notifyOnDataAvailable(true);
+        } catch (Exception e) {
+//            System.err.println(e.toString());
+        }
+    }
+
+    Automaat(){
+        initialize();
+//        Thread t=new Thread() {
+//            public void run() {
+//                //the following line will keep this app alive for 10 seconds,
+//                //waiting for events to occur and responding to them (printing incoming messages to console).
+//                try {Thread.sleep(10000);} catch (InterruptedException ie) {}
+//                System.exit(0);
+//            }
+//        };
+//        t.start();
+    }
+
+    public static void main(String args[]) {
+        new Automaat();
+    }
+
+    public void serialEvent(SerialPortEvent spe) {
+        if (spe.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+            try {
+                String inputLine=input.readLine();
+                System.out.println(inputLine);
+            } catch (Exception e) {
+//                System.err.println(e.toString());
+            }
+        }
+    }
+
+}
